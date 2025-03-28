040089dcef2978b999e8b1fd44a22b195c83c434
dungbh01103 <dungttbh01103#fpt.edu.vn>
2025-03-28 18:29:15 +0700

    update

--- Repository/IUserRepository.cs
+++ Repository/IUserRepository.cs
@@ -4,6 +4,9 @@ namespace WEB_Student.Repository
 {
     public interface IUserRepository
     {
-       
+        User GetUserByUsername(string username);
+        void AddUser(User user);
+
+        User AuthenticateUser(string username, string password);
     }
 }
