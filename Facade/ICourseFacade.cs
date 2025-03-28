7db52c06d7c2cb0561c9350bbf257dc78380d61f
luongbh01088 <luongtvdbh01088>
2025-03-28 22:47:30 +0700

    code len

--- Facade/ICourseFacade.cs
+++ Facade/ICourseFacade.cs
@@ -4,5 +4,10 @@ namespace WEB_Student.Facade
 {
     public interface ICourseFacade
     {
+        IEnumerable<Course> GetAllCourses();
+        Course GetCourseById(int id);
+        void AddCourse(Course course);
+        void UpdateCourse(Course course);
+        void DeleteCourse(int id);
     }
 }
