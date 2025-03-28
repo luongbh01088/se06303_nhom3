3db4940ff547689accaf6f3231d6eaafa967530a
dungbh01103 <dungttbh01103#fpt.edu.vn>
2025-03-28 18:19:58 +0700

    updatelen

--- Facade/IUserFacade.cs
+++ Facade/IUserFacade.cs
@@ -4,5 +4,9 @@ namespace WEB_Student.Facade
 {
     public interface IUserFacade
     {
+        string RegisterUser(string username, string email, string password, int roleId, string fullName, string phone);
+        void Register(User user);
+
+        User LoginUser(string username, string password);
     }
 }
